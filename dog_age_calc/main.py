9eba49e410804f9cfc3c9394dcb33b3055442c8f
Mecke Dev <crafted.mecke@gmail.com>
2022-05-21 12:40:15 +0200

    init

---
+++ dog_age_calc/main.py
@@ -0,0 +1,24 @@
+def age_calc(d_age):
+
+    try:
+        d_age = float(d_age)
+    except:
+        return f"{d_age} is not a valid Number."
+
+    if d_age > 0:
+        if d_age <= 1:
+            h_age = d_age*15
+        elif d_age <= 2:
+            h_age = d_age*12
+        elif d_age <= 1:
+            h_age = d_age*9.3
+        elif d_age <= 1:
+            h_age = d_age*8
+        elif d_age <= 1:
+            h_age = d_age*7.2
+        else:
+            h_age = 36+(d_age-5)*7
+
+        return f"The given Dog age {round(d_age,2)} is {round(h_age,2)} in human Years."
+
+print(age_calc(input("How old is your Dog?\nEnter age in Years: ")))
---
+++ dog_age_calc/readme.md
@@ -0,0 +1,2 @@
+Calculate Dog-Age into Human Years.
+did not check if the Calculation is correct, just used the Formula of the User on facebook.
