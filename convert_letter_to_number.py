5bee4a2243ac1b641d9f8e5f8d75af91d37aa3c3
Mecke Dev <crafted.mecke@gmail.com>
2022-06-10 13:46:43 +0200

    init

---
+++ convert_letter_to_number.py
@@ -0,0 +1,29 @@
+""" Module to convert Letters to Numbers """
+
+
+def convert_to_number(user_input):
+    """ converts a Letter to a Number """
+
+    letters = {
+        "A": 4,
+        "B": 3,
+        "C": 2
+    }
+    """ Dictionary to translate Letters to Numbers """
+
+    new_value = [str(letters[x.upper()])
+                 if x.upper() in letters
+                 else "-INVALID-"
+                 for x in user_input.split(",")
+                 ]
+    """ generating a List of Numbers for each Letter we have sent """
+
+    #  returning a String containing all Numbers we have converted
+    return ", ".join(new_value)
+
+
+#  getting User-Input containing Letters to convert
+user = input("Enter your Letter: ")
+
+#  printing the Results
+print(convert_to_number(user))
